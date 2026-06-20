efe04fe8b97fff328cb8c7c2f66e3d648ef97c0b
Logix <logix@example.com>
2026-06-20 09:51:43 -0400

    Migrate device tree to LineageOS 23.0 (Android 16)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_metroid.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_metroid-ap2a-user \
-    lineage_metroid-ap2a-userdebug \
-    lineage_metroid-ap2a-eng
+    lineage_metroid-bp2a-user \
+    lineage_metroid-bp2a-userdebug \
+    lineage_metroid-bp2a-eng
