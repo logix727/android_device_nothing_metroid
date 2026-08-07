b21bd1b33dae496d4b90f92d5dfaa61ff6b5b2a6
logix727 <mbstober@gmail.com>
2026-08-07 18:24:22 -0400

    Revert "metroid: register Qualcomm LPA compatibility library"
    This reverts commit 17246df28f04cac7fbb8d4a601828314223cb63b.

--- device.mk
+++ device.mk
@@ -89,7 +89,6 @@ PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper-prd \
     qti_telephony_hidl_wrapper_prd.xml \
     uimlpalibrary \
-    uimlpalibrary.xml \
     default-permissions-com.google.android.euicc.xml \
     privapp-permissions-com.google.android.euicc.xml
 
