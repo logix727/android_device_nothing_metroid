4d9bce00f670e33bff195096f30aec6de2ae3ea0
logix727 <mbstober@gmail.com>
2026-08-07 11:23:01 -0400

    metroid: fix Google LPA slot mapping

--- device.mk
+++ device.mk
@@ -85,6 +85,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # privileged EuiccService while EuiccPolicy provides Lineage partner policy.
 PRODUCT_PACKAGES += \
     EuiccPolicy \
+    MetroidEuiccPartner \
     default-permissions-com.google.android.euicc.xml \
     privapp-permissions-com.google.android.euicc.xml
 
---
+++ euiccpartner/src/org/lineageos/euiccpartner/PartnerReceiver.kt
@@ -0,0 +1,16 @@
+package com.android.euicc.partner
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class PartnerReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Received partner customization request")
+    }
+
+    private companion object {
+        const val TAG = "MetroidEuiccPartner"
+    }
+}
